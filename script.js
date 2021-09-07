288b2b918226a34109f3dc327a187957d8461930
Michael Chong <mikec0920@gmail.com>
2021-09-06 22:20:05 -0700

    Implemented simple 2 number calculations

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+let currentValue = 0;
+let lastValue = 0;
+let currentOperator = null;
+
 const add = function(num1, num2) {
-  return num1 + num2;
+  return parseFloat(num1) + parseFloat(num2);
 };
 
 const subtract = function(num1, num2) {
@@ -27,3 +31,54 @@ const operate = function(operator, num1, num2) {
   };
 };
 
+function display() {
+  const display = document.querySelector('#display');
+  const nums = document.querySelectorAll('.number');
+  nums.forEach(num => {
+    num.addEventListener('click', () => {
+      currentValue = (currentValue === 0) ? num.textContent : currentValue + num.textContent;
+      display.textContent = currentValue;
+    });
+  });
+
+  const operators = document.querySelectorAll('.operator');
+  operators.forEach(operator => {
+    operator.addEventListener('click', function(e) {
+      switch(e.target.id) {
+        case 'clear':
+          currentValue = 0;
+          lastValue = 0;
+          currentOperator = null;
+          display.textContent = 0;
+          break;
+        case 'divide':
+          lastValue = currentValue;
+          currentValue = 0;
+          currentOperator = '/';
+          break;
+        case 'multiply':
+          lastValue = currentValue;
+          currentValue = 0;
+          currentOperator = 'x';
+          break;
+        case 'subtract':
+          lastValue = currentValue;
+          currentValue = 0;
+          currentOperator = '-';
+          break;
+        case 'add':
+          lastValue = currentValue;
+          currentValue = 0;
+          currentOperator = '+';
+          break;
+        case 'equal':
+          lastValue = operate(currentOperator, lastValue, currentValue);
+          currentValue = lastValue;
+          display.textContent = lastValue;
+          break;
+      }
+    });
+  });
+}
+
+display();
