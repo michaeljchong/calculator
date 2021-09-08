138081590c392a15c428d544f421da2eef6eece5
Michael Chong <mikec0920@gmail.com>
2021-09-07 21:54:00 -0700

    Error message for division by zero

--- script.js
+++ script.js
@@ -43,81 +43,94 @@ const operate = function(operator, num1, num2) {
 const displayNum = function(num) {
   if (values.current && values.current.toString().includes('.')) {
     decimalPt.disabled = true;
-    console.log('deci');
     if (num.target.id === 'point') return;
   }
 
   values.current = (!values.current) ?
-                  num.target.textContent :
-                  values.current + num.target.textContent;
+          num.target.textContent :
+          values.current + num.target.textContent;
   display.textContent = values.current;
   document.querySelector('#clear').textContent = 'C';
+};
+
+const clear = function() {
+  values.current = null;
+  values.previous = null;
+  values.currentOperator = null;
+  display.textContent = 0;
+  decimalPt.disabled = false;
+  document.querySelector('#clear').textContent = 'AC';
+};
+
+const backspace = function() {
+  if (values.current) {
+    if (values.current.toString().includes('.')) {
+      decimalPt.disabled = false;
+    }
+    values.current = values.current.toString().slice(0, -1);
+  };
+  display.textContent =
+    (values.current === '' || values.current === null) ?
+      0 : values.current;
 };
 
 const equate = function() {
   if (values.currentOperator && values.current && values.previous) {
     values.current = operate(
-                      values.currentOperator,
-                      values.previous,
-                      values.current);
+            values.currentOperator,
+            values.previous,
+            values.current);
     display.textContent = values.current;
   };
 };
 
+const divisionByZero = function() {
+  if (values.currentOperator === '/' && values.current === '0') {
+    clear();
+    display.textContent = 'Error - Cannot divide by zero';
+  };
+}
+
 const useOperator = function(operator) {
   switch (operator.target.id) {
     case 'clear':
-      values.current = null;
-      values.previous = null;
-      values.currentOperator = null;
-      display.textContent = 0;
-      decimalPt.disabled = false;
-      document.querySelector('#clear').textContent = 'AC';
+      clear();
       break;
     case 'negative':
       if (values.current) values.current *= -1;
       display.textContent = values.current;
       break;
-    case 'delete':
-      if (values.current) {
-        if (values.current.toString().includes('.')) decimalPt.disabled = false;
-        values.current = values.current.toString().slice(0, -1);
-      }
-      display.textContent = (values.current === '' || values.current === null) ?
-                            0 : values.current;
+    case 'backspace':
+      backspace();
       break;
     case 'equal':
-      equate()
+      divisionByZero();
+      equate();
       values.currentOperator = null;
       break;
     default:
       // operator.target.classList.add('selectedOperator');
+      divisionByZero();
       if (values.currentOperator) equate();
       values.previous = values.current;
       values.current = null;
       values.currentOperator = operator.target.textContent;
       decimalPt.disabled = false;
-      console.log('oper');
       break;
   };
 };
 
-function calculator() {
-  const nums = document.querySelectorAll('.number');
-  nums.forEach(num => {
-    num.addEventListener('click', displayNum);
-  });
+const nums = document.querySelectorAll('.number');
+nums.forEach(num => {
+  num.addEventListener('click', displayNum);
+});
 
-  const operators = document.querySelectorAll('.operator');
-  operators.forEach(operator => {
-    operator.addEventListener('click', useOperator);
-  });
-};
-
-calculator();
+const operators = document.querySelectorAll('.operator');
+operators.forEach(operator => {
+  operator.addEventListener('click', useOperator);
+});
 
 /* TO DO
-- division by 0 should result in an error message
 - input after hitting equals should clear and start a new computation sequence, 
     operator after equals should continue the current sequence
 - selected operator changes color while in use
